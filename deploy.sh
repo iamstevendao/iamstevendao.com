522cb9f35a5ea4c26568a9d5e3bbefbd9bbb3c40
Steven Dao <iamstevendao@gmail.com>
2018-09-02 23:30:27 +1000

    Fix deployment script to run in CircleCI (attempt)

--- deploy.sh
+++ deploy.sh
@@ -11,6 +11,8 @@ cd dist
 
 # if you are deploying to a custom domain
 # echo 'www.example.com' > CNAME
+git config --global user.email "$GH_EMAIL" > /dev/null 2>&1
+git config --global user.name "$GH_NAME" > /dev/null 2>&1
 
 git init
 git add -A
