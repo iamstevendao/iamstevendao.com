3f29abf9e63227970022b368da442d2a87d67da6
Steven Dao <iamstevendao@gmail.com>
2018-09-03 00:58:41 +1000

    Ignore gh-pages using new deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,27 +1,55 @@
-#!/usr/bin/env sh
+#!/bin/sh
+# ideas used from https://gist.github.com/motemen/8595451
 
-# abort on errors
+# abort the script if there is a non-zero error
 set -e
 
-# build
-npm run build
+# show where we are on the machine
+pwd
 
-# navigate into the build output directory
-cd dist
+remote=$(git config remote.origin.url)
 
-# if you are deploying to a custom domain
-# echo 'www.example.com' > CNAME
+siteSource="$1"
+
+if [ ! -d "$siteSource" ]
+then
+    echo "Usage: $0 <site source dir>"
+    exit 1
+fi
+
+# make a directory to put the gp-pages branch
+mkdir gh-pages-branch
+cd gh-pages-branch
+# now lets setup a new repo so we can update the gh-pages branch
 git config --global user.email "$GH_EMAIL" > /dev/null 2>&1
 git config --global user.name "$GH_NAME" > /dev/null 2>&1
-
 git init
+git remote add --fetch origin "$remote"
+
+# switch into the gh-pages branch
+if git rev-parse --verify origin/gh-pages > /dev/null 2>&1
+then
+    git checkout gh-pages
+    # delete any old site as we are going to replace it
+    # Note: this explodes if there aren't any, so moving it here for now
+    git rm -rf .
+else
+    git checkout --orphan gh-pages
+fi
+
+# copy over or recompile the new site
+cp -a "../${siteSource}/." .
+
+# stage any changes and new files
 git add -A
-git commit -m "Deploy"
+# now commit, ignoring branch gh-pages doesn't seem to work, so trying skip
+git commit --allow-empty -m "Deploy to GitHub pages [ci skip]"
+# and push, but send any output to /dev/null to hide anything sensitive
+git push --force --quiet origin gh-pages > /dev/null 2>&1
 
-# if you are deploying to https://<USERNAME>.github.io
-# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
+# go back to where we started and remove the gh-pages git repo we made and used
+# for deployment
+cd ..
+rm -rf gh-pages-branch
 
-# if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:iamstevendao/iamstevendao.com.git master:gh-pages
-
-cd -
+echo "Finished Deployment!"
