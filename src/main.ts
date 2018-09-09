18c82033e0d5ec8d42311fe8e6841bd5054850ce
Steven Dao <iamstevendao@gmail.com>
2018-09-09 23:07:18 +1000

    Fix typo

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import './config/circle.yml'; // Circle CI config to ignore gh-pages branch (workaround)
 import './config/robots.txt'; // For Google bot to crawl the web
-import './config/sidemap.xml'; // For Google bot to crawl the web
+import './config/sitemap.xml'; // Sitemap
 
 import Vue from 'vue';
 import App from './App.vue';
