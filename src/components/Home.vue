f4dd05f9fc5a2d83eade0ce5f7dba3fbf17999d2
Steven Dao <iamstevendao@gmail.com>
2019-06-09 00:18:49 +1000

    Change copy

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -3,29 +3,29 @@
 <template>
   <div class="home">
     <a href="#">
-      <img src="../assets/avatar.jpg"
-           style="border-radius: 50%; width: 25%; min-width: 80px; max-width: 180px;"
-           alt="Steven Dao's avatar">
+      <img
+        src="../assets/avatar.jpg"
+        style="border-radius: 50%; width: 25%; min-width: 80px; max-width: 180px;"
+        alt="Steven Dao's avatar"
+      />
     </a>
     <h1>Hi, I'm Steven Dao.</h1>
     <p>
-      I'm building webs with my friends at
-      <a href="https://geteducation.link"
-         target="_blank"
-         rel="noopener">EducationLink</a>.
+      I'm building
+      <a href="https://geteducation.link" target="_blank" rel="noopener">EducationLink</a>
+      with the most awesome people on Earth.
     </p>
-    <p>I develop, maintain open source projects <br> and am a bit obsessed with dogs and cats.</p>
-    <h3>Ping me at</h3>
+    <p>
+      I create, maintain open source projects <br />
+      and am a bit obsessed with dogs and cats.
+    </p>
+    <h3>Find me at</h3>
     <ul>
       <li>
-        <a href="https://github.com/iamstevendao"
-           target="_blank"
-           rel="noopener">Github</a>
+        <a href="https://github.com/iamstevendao" target="_blank" rel="noopener">Github</a>
       </li>
       <li>
-        <a href="https://twitter.com/iamstevendao"
-           target="_blank"
-           rel="noopener">Twitter</a>
+        <a href="https://twitter.com/iamstevendao" target="_blank" rel="noopener">Twitter</a>
       </li>
     </ul>
   </div>
