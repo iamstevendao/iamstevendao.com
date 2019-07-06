646159e41216c10ac8c7f1c3d4b76351c71b1c13
Steven Dao <iamstevendao@gmail.com>
2019-07-06 22:18:16 +1000

    Remove custom domain in deploy script

--- deploy.sh
+++ deploy.sh
@@ -9,8 +9,6 @@ npm run build
 # navigate into the build output directory
 cd dist
 
-echo 'www.iamstevendao.com' > CNAME
-
 git config --global user.email "$GH_EMAIL" > /dev/null 2>&1
 git config --global user.name "$GH_NAME" > /dev/null 2>&1
 
