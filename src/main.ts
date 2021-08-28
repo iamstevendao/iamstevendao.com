37cce04f5757c7fd31460c430dffd3b4c2ca508b
Steven Dao <iamstevendao@gmail.com>
2021-08-29 00:46:53 +1000

    build: Move build from circle ci to netlify

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,3 @@
-import './config/circle.yml'; // Circle CI config to ignore gh-pages branch (workaround)
 import './config/robots.txt'; // For Google bot to crawl the web
 import './config/sitemap.xml'; // Sitemap
 import './assets/css/theme.css';
