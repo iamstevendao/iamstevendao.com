ce7eb68c5f8be6b4450fdb906ce000d161bfed26
Steven Dao <iamstevendao@gmail.com>
2024-05-05 19:21:07 +1000

    feat: Add icon to metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,15 @@
 import type { Metadata } from 'next'
 import { Open_Sans } from 'next/font/google'
 import './globals.css'
-import Head from 'next/head'
 
 const openSans = Open_Sans({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: 'Steven Dao',
   description: 'Technical Lead/Full Stack Dev',
+  icons: {
+    icon: '/favicon.ico',
+  },
 }
 
 export default function RootLayout({
@@ -17,9 +19,6 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <Head>
-        <link rel="icon" href="/favicon.ico" sizes="any" />
-      </Head>
       <body className={`${openSans.className} dark:bg-vscode-bg dark:text-slate-300`}>{children}</body>
     </html>
   )
