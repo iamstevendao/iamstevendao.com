797ac917a13d5ad40d9668c243dd9cd001c57624
Steven Dao <iamstevendao@gmail.com>
2024-05-05 18:58:34 +1000

    feat: Add favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next'
 import { Open_Sans } from 'next/font/google'
 import './globals.css'
+import Head from 'next/head'
 
 const openSans = Open_Sans({ subsets: ['latin'] })
 
@@ -16,6 +17,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <Head>
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+      </Head>
       <body className={`${openSans.className} dark:bg-vscode-bg dark:text-slate-300`}>{children}</body>
     </html>
   )
